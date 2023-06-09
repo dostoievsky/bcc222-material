0e99a66a7a999e88d3b80b670d0c49bfeb3426e4
dostoievsky <bernardo.emery@gmail.com>
2023-06-09 17:53:45 -0300

    chore: class 03 exercise

--- aula03/Aula03.hs
+++ aula03/Aula03.hs
@@ -8,3 +8,21 @@ foo (x : xs) = x * foo xs
 
 instance Num String where
   x * y = show $ (length x) * (length y)
+
+bools :: [Bool]
+bools = [True, True, False, True, False, False]
+
+nums  :: [[Int]]
+nums = [[1,2,3,4], [5,6,7,8]]
+
+add   :: Int -> Int -> Int -> Int
+add x y z = x + y + z
+
+copy  :: a -> (a, a)
+copy a = (a, a)
+
+-- apply :: (a -> b) -> a -> b
+-- apply (a -> b) = a b
+
+swap  :: (a,b) -> (b,a)
+swap (a, b) = (b, a)
