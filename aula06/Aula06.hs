595c2725ca32078bc1ca67e40cfaadc69cb0d0db
base <git@bernardoemery.com>
2023-06-12 18:30:59 -0300

    Aula06 (#8)
    * chore: class01 study
    
    * chore: class 2 exercices
    
    * chore: class 04 exercise
    
    * chore: class 04
    
    * chore: class 05
    
    * chore: class 06
    
    ---------
    
    Co-authored-by: dostoievsky <bernardo.emery@gmail.com>

--- aula06/Aula06.hs
+++ aula06/Aula06.hs
@@ -1,16 +1,10 @@
 module Aula06 where
 
-import Prelude hiding ( foldr
-                      , foldl
-                      , sum
-                      , concat
-                      , and
-                      , length
-                      , map
-                      , reverse
-                      , filter
-                      , (++)
-                      )
+import Prelude hiding ( 
+  foldr, foldl, sum, concat, 
+  and, length, map, reverse, 
+  filter, takeWhile, all,
+  concatMap)
 
 foldr :: (a -> b -> b) -> b -> [a] -> b
 foldr _ v []       = v
@@ -44,3 +38,34 @@ filter :: (a -> Bool) -> [a] -> [a]
 filter p = foldr step []
    where
      step x ac = if p x then x : ac else ac
+
+takeWhile :: (a -> Bool) -> [a] -> [a]
+takeWhile _ []  = []
+takeWhile p (x : xs)
+    | p x       = x : takeWhile p xs
+    | otherwise = []
+
+takeWhileF :: (a -> Bool) -> [a] -> [a]
+takeWhile f = foldr step []
+  where
+    step x ac = if f x then x : ac else ac  
+
+all :: (a -> Bool) -> [a] -> Bool
+all _ []      = True
+all p (x : xs)
+  | p x       = all p xs
+  | otherwise = False
+
+allF :: (a -> Bool) -> [a] -> Bool
+all f = foldr step True
+  where
+    step x xs = if f x then xs else False  
+
+-- concatMap :: (a -> [b]) -> [a] -> [b]
+-- concatMap _ [] = []
+-- concatMap f xs ys
+
+-- concatMapF :: (a -> [b]) -> [a] -> [b]
+-- concatMapF f = foldr step []
+--   where
+--     step   
